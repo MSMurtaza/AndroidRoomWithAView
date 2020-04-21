44cc3f8403413d54f47b08715ef582739e9a2e94
MSMurtaza <shoaibmurtaza1@yahoo.com>
2020-04-22 01:14:03 +0500

    Connect with the data

--- app/src/main/java/android/example/androidroomwithaview/MainActivity.java
+++ app/src/main/java/android/example/androidroomwithaview/MainActivity.java
@@ -1,12 +1,24 @@
 package android.example.androidroomwithaview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+    private WordViewModel mWordViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +29,35 @@ public class MainActivity extends AppCompatActivity {
         final WordListAdapter wordListAdapter = new WordListAdapter(this);
         recyclerView.setAdapter(wordListAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mWordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+        mWordViewModel.getmAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(List<Word> words) {
+                // update the cached copy of the words in the adapter
+                wordListAdapter.setmWords(words);
+            }
+        });
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,  resultCode,  data);
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE
+        && resultCode == RESULT_OK) {
+            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            mWordViewModel.insert(word);
+        } else {
+            Toast.makeText(getApplicationContext(),  R.string.empty_not_saved, Toast.LENGTH_LONG).show();
+        }
     }
 }
